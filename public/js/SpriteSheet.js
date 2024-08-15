82ddc128277842c4cd0bb550fa5647dc7d27d6c2
Fernando Gonçalves <fernando_jcg22@hotmail.com>
2024-08-15 12:38:58 +0000

    JS: Loading images and JSON asynchronously / ES6 Import/Export /  Using canvas to save image buffers

---
+++ public/js/SpriteSheet.js
@@ -0,0 +1,36 @@
+export default class SpriteSheet {
+    constructor(image, w = 16, h = 16) {
+        this.image = image;
+        this.width = w;
+        this.height = h;
+        this.tiles = new Map();
+    }
+
+    define(name, x, y) {
+        const buffer = document.createElement('canvas');
+        buffer.height = this.height;
+        buffer.width = this.width;
+        buffer
+            .getContext('2d')
+            .drawImage(
+                this.image,
+                this.width * x,
+                this.height * y,
+                this.width,
+                this.height,
+                0,
+                0,
+                this.width,
+                this.height);
+        this.tiles.set(name, buffer);
+    }
+
+    draw(name, context, x, y) {
+        const buffer = this.tiles.get(name);
+        context.drawImage(buffer, x, y);
+    }
+
+    drawTile(name, context, x, y) {
+        this.draw(name, context, x * this.width, y * this.height);
+    }
+}
