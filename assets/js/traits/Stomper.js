5dc72ae1393d37ce86fece92436f5f5f497e7bf0
Fernando Gonçalves <fernando_jcg22@hotmail.com>
2024-08-16 16:27:46 +0000

    DOCS: Change the sounds fiele, path to game.html and add js for sounds images and video, sounds config.."

---
+++ assets/js/traits/Stomper.js
@@ -0,0 +1,28 @@
+import {Sides, Trait} from '../Entity.js';
+
+export default class Stomper extends Trait {
+    constructor() {
+        super('stomper');
+        this.bounceSpeed = 400;
+
+        this.onStomp = function() {
+        }
+    }
+
+    bounce(us, them) {
+        us.bounds.bottom = them.bounds.top;
+        us.vel.y = -this.bounceSpeed;
+    }
+
+    collides(us, them) {
+        if (!them.killable || them.killable.dead) {
+            return;
+        }
+
+        if (us.vel.y > them.vel.y) {
+            this.bounce(us, them);
+            this.sounds.add('stomp');
+            this.onStomp(us, them);
+        }
+    }
+}
